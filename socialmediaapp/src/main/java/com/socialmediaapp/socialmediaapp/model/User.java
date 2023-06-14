9a1300c62fe9383bb90fa8650ba98a45de8338fb
Rishanan-A <126557378+Rishanan-A@users.noreply.github.com>
2023-06-14 10:48:01 +0800

    Social Media App Backbone

---
+++ socialmediaapp/src/main/java/com/socialmediaapp/socialmediaapp/model/User.java
@@ -0,0 +1,51 @@
+package com.socialmediaapp.socialmediaapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id;
+    private String username;
+    private String password;
+    private String email;
+    private String role = "User";
+    // getter and setter methods
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+    
+    
+}
