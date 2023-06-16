38c8a696d976e3f20073bd164a5be0f043b0c822
Emmanuel Lim <limjunhao5714@gmail.com>
2023-06-16 15:42:18 +0800

    Handle online upload to Firebase

--- social-media-react-app/src/components/CreateProfilePost.js
+++ social-media-react-app/src/components/CreateProfilePost.js
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import React  from 'react'
-import NavBar from './NavBar'
-import { Button, Container,Form,Row,Col} from 'react-bootstrap'
+import { Button, Container,Form} from 'react-bootstrap'
 import axios from 'axios'
-
+import { getDownloadURL,getStorage, ref, uploadBytes } from 'firebase/storage';
+import { app } from "./Firebase"
 
 function CreateProfilePost() {
 
     const userObject = localStorage.getItem('user')
     const username = localStorage.getItem('username')
+    const storage = getStorage();
 
     const getCurrentTimestamp = () => {
       const date = new Date();
@@ -30,8 +31,27 @@ function CreateProfilePost() {
       mediaUrl:'',
       createdAt:getCurrentTimestamp()
     });
+   
+    //Event to handle 
+    const handleFileUpload = async (event) => {
+      const file = event.target.files[0];
+      const storageRef = ref(storage, 'gs://springtest-f7ba8.appspot.com/' + file.name);
+      
+      try {
+        await uploadBytes(storageRef, file);
+        console.log('File uploaded successfully!');
+        const downloadURL = await getDownloadURL(storageRef);
+        console.log('File download URL:', downloadURL)
 
-
+        //From handleChange
+        setFormData({
+          ...formData,
+        [event.target.name]: downloadURL,
+        });
+      } catch (error) {
+        console.error('Error uploading file:', error);
+      }
+    };
     
 
     const handleChange = (event) => {
@@ -45,6 +65,17 @@ function CreateProfilePost() {
       const handleSubmit = (event) => {
         event.preventDefault();
     
+        //--------------------
+
+        //Merging with Firebase:
+        //String of file link from Firebase should be set in FormData
+        //Currently it is holding String pathname to file w/ fakepath
+        
+        //Either from here, upload to Firebase, receive URL from Firebase, setFormData
+        //or upload to Firebase upon handleFileUpload, setFormData with URL received from Firebase
+
+        //--------------------
+
         // Update the existing object with the form values
         //formData.user = user;
         setFormData({ ...formData, user: userObject });
@@ -68,16 +99,23 @@ function CreateProfilePost() {
         <Form onSubmit={handleSubmit}>
           <Form.Group controlId="caption">
             <Form.Label>Caption</Form.Label>
-            <Form.Control type="text" name="caption" placeholder="Enter Name" onChange={handleChange} />
+            <Form.Control type="text" name="caption" required placeholder="Enter Name" onChange={handleChange} />
           </Form.Group>
           <Form.Group controlId="content">
             <Form.Label>Content</Form.Label>
-            <Form.Control type="text" name="content" placeholder="Enter Content" onChange={handleChange}/>
+            <Form.Control type="text" name="content" required placeholder="Enter Content" onChange={handleChange}/>
           </Form.Group>
+{/* New mediaURL (select file to upload to FireBase) */}
           <Form.Group controlId="mediaUrl">
             <Form.Label>Media URL</Form.Label>
-            <Form.Control type="text" name="mediaUrl" placeholder="Enter URL" onChange={handleChange}/>
+            <Form.Control type="file" name="mediaUrl" placeholder="Enter URL" onChange={handleFileUpload}/>
           </Form.Group>
+
+{/* Old mediaURL (insert your own URL) */}
+          {/* <Form.Group controlId="mediaUrl">
+            <Form.Label>Media URL</Form.Label>
+            <Form.Control type="text" name="mediaUrl" placeholder="Enter URL" onChange={handleChange}/>
+          </Form.Group> */}
           <Button type="submit" variant="primary" className="btn-block rounded-pill mb-3">Submit</Button>
         </Form>
   </Container>
